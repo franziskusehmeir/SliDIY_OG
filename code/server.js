71c122bda2540f5200f6ff71aaae25dfcb982154
franziskusehmeir <38812519+franziskusehmeir@users.noreply.github.com>
2020-06-22 09:23:19 +0200

    Update server.js

--- code/server.js
+++ code/server.js
@@ -33,5 +33,5 @@ var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://localhost:8081");
 })
