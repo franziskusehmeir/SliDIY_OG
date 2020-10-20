5a670f0b23a682158158ec86ea13312537724752
Leon Tea <leon.tea@hotmail.com>
2020-10-20 09:08:09 +0200

    add unlock function

--- code/server.js
+++ code/server.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
+   res.sendFile(__dirname + "/" + "index.htm");
 })
 
 app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
    response = {
-      command:req.query.comm
+      command: req.query.comm
    };
    console.log(response);
    console.log(req.query.comm);
@@ -23,6 +23,28 @@ app.get('/process_get', function (req, res) {
    stty.on('close', () => {
       //exec('echo "G0X40" > /dev/' + portDescriptor);
       exec('echo "' + req.query.comm + '" > /dev/' + portDescriptor);
+   });
+
+   res.end(JSON.stringify(response));
+
+})
+
+app.get('/process_unlock', function (req, res) {
+
+   const unlockCommand = '$20=0'
+
+   response = {
+      command: unlockCommand
+   };
+   console.log(response);
+   console.log(req.query.comm);
+
+   const { spawn, exec } = require('child_process')
+   const portDescriptor = process.env.PORT_DESCRIPTOR || 'ttyUSB0'
+
+   const stty = spawn('stty', ['-F', '/dev/' + portDescriptor, '115200']);
+   stty.on('close', () => {
+      exec('echo "' + unlockCommand + '" > /dev/' + portDescriptor);
    });
 
    res.end(JSON.stringify(response));
