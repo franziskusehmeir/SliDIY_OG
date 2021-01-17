41e3d61bf91df61075b71e0b3d29e023779c04d8
tealeon <leon.tea@hotmail.com>
2021-01-17 23:36:44 +0100

    add standby-phase to stepper

--- schrittmotor.py
+++ schrittmotor.py
@@ -3,32 +3,28 @@ import time
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-coil_A_1_pin = 4 # pink
-coil_A_2_pin = 17 # orange
-coil_B_1_pin = 23 # blau
-coil_B_2_pin = 24 # gelb
-#enable_pin   = 7 # Nur bei bestimmten Motoren benoetigt (+Zeile 24 und 30)
- 
+coil_A_2_pin = 4 # A\ gruen-weiss
+coil_B_1_pin = 17 # B rot-lila
+coil_A_1_pin = 23 # A schwarz-schwarz
+coil_B_2_pin = 24 # B\ blau-grau
+
 # anpassen, falls andere Sequenz
 StepCount = 8
 Seq = list(range(0, StepCount))
-Seq[0] = [0,1,0,0]
-Seq[1] = [0,1,0,1]
-Seq[2] = [0,0,0,1]
+Seq[0] = [1,0,1,0]
+Seq[1] = [0,1,1,0]
+Seq[2] = [0,1,0,1]
 Seq[3] = [1,0,0,1]
 Seq[4] = [1,0,0,0]
 Seq[5] = [1,0,1,0]
 Seq[6] = [0,0,1,0]
 Seq[7] = [0,1,1,0]
  
-#GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
- 
-#GPIO.output(enable_pin, 1)
- 
+  
 def setStep(w1, w2, w3, w4):
     GPIO.output(coil_A_1_pin, w1)
     GPIO.output(coil_A_2_pin, w2)
@@ -49,6 +45,7 @@ def backwards(delay, steps):
             
 if __name__ == '__main__':
     while True:
+	setStep(0,0,0,0)
         delay = raw_input("Zeitverzoegerung (ms)?")
         steps = raw_input("Wie viele Schritte vorwaerts? ")
         forward(int(delay) / 1000.0, int(steps))
